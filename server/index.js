62626dd070b0eec1bf7af967451bee368c89f95f
ershavait <ershavait@gmail.com>
2026-08-30 02:47:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,20 +8,17 @@ import authRouter from "./routes/auth.route.js"
 import userRouter from "./routes/user.route.js"
 import interviewRouter from "./routes/interview.route.js"
 import paymentRouter from "./routes/payment.route.js"
-
+const app = express()
 app.use(cors({
-    origin:"https://interviewai-hmc5.onrender.com",
+    origin:"https://interviewai-hmc5.onrender.com/",
     credentials:true
 }))
-
 app.use(express.json())
 app.use(cookieParser())
-
 app.use("/api/auth" , authRouter)
 app.use("/api/user", userRouter)
 app.use("/api/interview" , interviewRouter)
 app.use("/api/payment" , paymentRouter)
-
 const PORT = process.env.PORT || 6000
 app.listen(PORT , ()=>{
     console.log(`Server running on port ${PORT}`)
